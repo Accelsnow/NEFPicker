6a90055076dcad7e3784ccfc522ba5b8bcd6c05b
Adrian Zhao <acceleratorsnow@gmail.com>
2025-03-05 13:35:02 -0500

    Add EXIF display

--- ImageHandler.py
+++ ImageHandler.py
@@ -6,8 +6,12 @@ import sys
 import shutil
 import glob
 import rawpy
+import exiftool
+import fractions
+from datetime import datetime
 from PIL import Image
 from CTkMessagebox import CTkMessagebox
+
 
 def disp_error(msg: str, exit_after: bool = False):
     msg = CTkMessagebox(title="Error", message=msg, icon="cancel")
@@ -15,12 +19,43 @@ def disp_error(msg: str, exit_after: bool = False):
     if exit_after:
         sys.exit(1)
 
+
+def read_meta(file: str):
+    with exiftool.ExifToolHelper() as et:
+        metadata = et.get_metadata([file])
+
+        shutter = ""
+        if "EXIF:ExposureTime" in metadata[0]:
+            ss = float(metadata[0]["EXIF:ExposureTime"])
+            shutter = str(fractions.Fraction(ss).limit_denominator())
+            if ss >= 1:
+                shutter += '"'
+
+        aper = f"f/{metadata[0]['EXIF:FNumber']}" if "EXIF:FNumber" in metadata[0] else ""
+        iso = f"ISO{metadata[0]['EXIF:ISO']}" if "EXIF:ISO" in metadata[0] else ""
+        ev = f"{metadata[0]['EXIF:ExposureCompensation']}EV" if "EXIF:ExposureCompensation" in metadata[0] else ""
+        foc = f"{metadata[0]['EXIF:FocalLength']}mm" if "EXIF:FocalLength" in metadata[0] else ""
+        date_str = datetime.strptime(metadata[0]["EXIF:DateTimeOriginal"],
+                                     "%Y:%m:%d %H:%M:%S") if "EXIF:DateTimeOriginal" in metadata[0] else ""
+        lens = f"{metadata[0]['EXIF:LensModel']}" if "EXIF:LensModel" in metadata[0] else ""
+        return {
+            "shutter": shutter,
+            "aper": aper,
+            "iso": iso,
+            "ev": ev,
+            "foc": foc,
+            "date": date_str,
+            "lens": lens,
+        }
+
+
 class ImageObject:
     def __init__(self, nef_file: str = None, jpg_file: str = None):
         self._valid = False
         self.nef_file = nef_file
         self.jpg_file = jpg_file
         self.pil = None
+        self.meta = None
         self.info = None
         self.prev = None
         self.next = None
@@ -35,7 +70,8 @@ class ImageObject:
                 elif thumb.format == rawpy.ThumbFormat.BITMAP:
                     self.pil = Image.fromarray(thumb.data)
                 else:
-                    msg = CTkMessagebox(title="Unknown NEF Thumb Format", message=f"Unsupported thumbnail format {str(thumb.format)} in NEF file: {self.nef_file}.",
+                    msg = CTkMessagebox(title="Unknown NEF Thumb Format",
+                                        message=f"Unsupported thumbnail format {str(thumb.format)} in NEF file: {self.nef_file}.",
                                         icon="warning", option_1="Exit", option_2="Continue")
                     if msg.get() == "Exit":
                         sys.exit(1)
@@ -43,17 +79,26 @@ class ImageObject:
         else:
             raise ValueError("Neither NEF nor JPEG file is present.")
 
+        if self.nef_file is not None:
+            self.meta = read_meta(self.nef_file)
+        elif self.jpg_file is not None:
+            self.meta = read_meta(self.jpg_file)
+        else:
+            raise ValueError("Neither NEF nor JPEG file is present.")
+
+        self.info = f"{self.meta['shutter']}  |  {self.meta['aper']}  |  {self.meta['iso']}  |  {self.meta['ev']}  |  {self.meta['foc']}  |  {self.meta['lens']}  |  {self.meta['date']}  |  {self.pil.width} x {self.pil.height} {self.pil.mode}"
+
         if self.jpg_file is not None and self.nef_file is not None:
             self.mode = "NEF + JPG"
+            self.info = f"NEF & JPG : " + self.info
             self.filename = str(self.jpg_file + " | " + self.nef_file).replace("\\", "/")
-            self.info = f"NEF & JPG : {self.pil.width} x {self.pil.height} {self.pil.mode}"
         elif self.nef_file is not None:
             self.mode = "NEF ONLY"
-            self.info = f"NEF : {self.pil.width} x {self.pil.height} {self.pil.mode}"
+            self.info = f"NEF : " + self.info
             self.filename = self.nef_file.replace("\\", "/")
         elif self.jpg_file is not None:
             self.mode = "JPG ONLY"
-            self.info = f"JPG : {self.pil.width} x {self.pil.height} {self.pil.mode}"
+            self.info = f"JPG : " + self.info
             self.filename = self.jpg_file.replace("\\", "/")
         else:
             raise ValueError("Neither NEF nor JPEG file is present.")
@@ -75,11 +120,14 @@ class ImageObject:
             self.pil = None
         return self._valid
 
+
 def is_jpg_file(filename: str) -> bool:
     return filename.upper().endswith(".JPG") or filename.upper().endswith(".JPEG")
 
+
 def is_nef_file(filename: str) -> bool:
     return filename.upper().endswith(".NEF")
+
 
 class ImageHandler:
     def __init__(self, nef_folder="./NEF", jpg_folder="./JPG", opt_nef_folder="./SEL_NEF", opt_jpg_folder="./SEL_JPG",
